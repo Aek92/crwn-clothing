a085f72ec8a47385106f5bcfa91b366ef57b7438
Aek92 <aek92.ak@gmail.com>
2022-02-23 17:58:36 +0100

    adding header component and links to home, shop page and contact

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,11 +9,11 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
-      className="background-image"
+      className='background-image'
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
